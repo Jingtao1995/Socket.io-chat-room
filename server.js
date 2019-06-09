768133c291bb7659a5dabb8dacf40b7bf1211248
Jingtao Cheng <jingtaocheng@Jingtaos-MacBook-Pro.local>
2019-06-08 19:13:11 -0700

    add features about room, -fix private chat problem

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var io = require('socket.io')(http);
 
 var currentUser = [];
 var currentSocket = [];
-var Rooms = ['room1'];
-var roomName = '';
+var currentUser_room = [];
+var Rooms = ['room1', 'room2', 'room3'];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -15,13 +15,24 @@ app.get('/', (req, res) => {
 io.on('connection', function (socket) {
     //if server receive socket with chat message flag, check receiver's name
     //and choose to send private msg or broadcast the msg to all
+
+    renewUserList();
+
     socket.on('chat message', (userName, receiverName, msg, date) => {
         if (receiverName == 'all') {
             //send message to all client without self
-            socket.broadcast.emit('chat message', userName, receiverName, msg, date);
+            socket.broadcast.to(currentSocket.room).emit('chat message', userName, receiverName, msg, date);
         }
         else {
-            currentSocket[currentUser.indexOf(receiverName)].emit('chat message', userName, receiverName, msg, date);
+            if (currentUser_room[currentUser.indexOf(receiverName)] == currentSocket.room) {
+                // console.log('receiver is in the same room');
+                currentSocket[currentUser.indexOf(receiverName)].emit('chat message', userName, receiverName, msg, date);
+            }
+            else {
+                // socket.broadcast.to(currentSocket.room).emit('error no user', receiverName, currentSocket.room);
+                socket.emit('error no user', receiverName, currentSocket.room);
+            }
+            // currentSocket[currentUser.indexOf(receiverName)].emit('chat message', userName, receiverName, msg, date);
         }
         console.log(date + ' - ' + userName + ' to ' + receiverName + ' : ' + msg);
     })
@@ -29,19 +40,29 @@ io.on('connection', function (socket) {
 
     //when server receive socket of 'new user' event, store user's name and
     //response to cient.
-    socket.on('new user', (userName) => {
+    socket.on('new user', (userName) => { //function(userName) {}
         if (currentUser.indexOf(userName) == -1) {
+
             currentUser.push(userName);
             currentSocket.push(socket);
-            io.emit('msg user join', userName);
-            io.emit('add userList', userName);
-            console.log(userName + " join room");
+            currentUser_room.push('room1');
+
+            currentSocket.room = 'room1';
+            socket.join('room1');
+
+            io.sockets.in(currentSocket.room).emit('msg user join', userName, currentSocket.room);
+            // io.sockets.in(currentSocket.room).emit('update userList', userName, currentSocket.room);
+            // io.emit('update userList', userName, currentSocket.room);
+            io.in(currentSocket.room).emit('update userList', userName, currentSocket.room);
+
+            console.log(userName + " join " + currentSocket.room);
+
+            renewUserList();
         }
         else {
             socket.emit('alert userName exists', userName);
         }
     })
-
 
     //when server listen disconnect event
     socket.on('disconnect', () => {
@@ -54,15 +75,42 @@ io.on('connection', function (socket) {
             //remove leaved user's name and socket
             currentUser.splice(currentSocket.indexOf(socket), 1);
             currentSocket.splice(currentSocket.indexOf(socket), 1);
+            currentUser_room.splice(currentSocket.indexOf(socket), 1);
         }
     })
 
-    //once new client connected to server add users to client List selector
-    for (var i = 0; i < currentUser.length; i++) {
-        socket.emit('add userList', currentUser[i]);
-        //console.log('USER ADD');
-    }
+    //when server listen clinet send swith room event
+    socket.on('switchRoom', (userName, currentRoom, newroom) => {
+        if (currentUser.indexOf(userName) == -1) {
 
+            currentUser.push(userName);
+            currentSocket.push(socket);
+
+        }
+
+        socket.join(newroom);
+        io.sockets.in(currentRoom).emit('msg user leave', currentUser[currentSocket.indexOf(socket)], currentSocket.room);
+
+        currentSocket.room = newroom;
+        currentUser_room[currentSocket.indexOf(socket)] = newroom;
+
+        io.sockets.in(newroom).emit('msg user join', currentUser[currentSocket.indexOf(socket)], currentSocket.room);
+        currentUser.room = newroom;
+
+        renewUserList()
+    })
+
+    //once new client connected to server add users to client List selector
+    function renewUserList() {
+        // console.log('loop start');
+        for (var i = 0; i < currentUser.length; ++i) {
+            io.sockets.emit('update userList', currentUser[i], currentUser_room[i]);
+            console.log('update ' + currentUser[i] + ' in ' + currentUser_room[i]);
+            // console.log("currentUser.length = " + currentUser.length);
+            // console.log('the ' + i + ' th; ');
+            // console.log(currentUser[i] + ' ... ' + currentUser_room[i] + ' ... ' + currentSocket.room);
+        }
+    }
 })
 
 http.listen(process.env.PORT || 3000, function () {
